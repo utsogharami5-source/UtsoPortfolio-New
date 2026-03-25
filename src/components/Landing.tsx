e4bf81dff084e5de6181a80018ccc64d025ba35d
Utso Gharami <utsogharami5@gmail.com>
2026-03-25 19:00:54 +0600

    Branding_Updated_Name_Utso_Gharami

--- src/components/Landing.tsx
+++ src/components/Landing.tsx
@@ -9,9 +9,7 @@ const Landing = ({ children }: PropsWithChildren) => {
           <div className="landing-intro">
             <h2>Hello! I'm</h2>
             <h1>
-              UTSO
-              <br />
-              <span>GHARAMI</span>
+              Utso <span>Gharami</span>
             </h1>
           </div>
           <div className="landing-info">
